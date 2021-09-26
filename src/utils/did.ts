3fc39edd3a0de11e62ee1742859d8e4772bba4ff
Coodos <info@coodos.co>
2021-09-26 20:49:43 +0530

    bip39 mnemonic works

--- src/utils/did.ts
+++ src/utils/did.ts
@@ -1,19 +1,37 @@
 import {
+  DID,
+  publish,
   Document,
   KeyType,
-  publish,
+  KeyPair,
 } from "@iota/identity-wasm/node/identity_wasm.js";
+import { Ed25519Seed, Bip39, Converter, Ed25519 } from "@iota/iota.js";
 import { createEncryptedVault } from "./stronghold";
+import bs58 from "bs58";
 
 const createIdentity = async (clientConfig: Object = {}) => {
-  const { doc, key }: any = new Document(KeyType.Ed25519);
+  const mnemonic = Bip39.randomMnemonic();
+  const baseSeed = Ed25519Seed.fromMnemonic(mnemonic);
+  const baseKeypair = baseSeed.keyPair();
+  const pubKey = bs58.encode(
+    Buffer.from(Converter.bytesToHex(baseKeypair.privateKey), "hex")
+  );
+  const privKey = bs58.encode(
+    Buffer.from(Converter.bytesToHex(baseKeypair.publicKey), "hex")
+  );
+  console.log(pubKey);
 
-  doc.sign(key);
+  const keyPair = KeyPair.fromBase58(KeyType.Ed25519, pubKey, privKey);
+  console.log(keyPair);
 
-  const messageId = await publish(doc.toJSON(), clientConfig);
-  await createEncryptedVault(key, "password", "key");
-
-  return { key, doc };
+  const doc = Document.fromKeyPair(keyPair);
+  console.log(doc);
 };
+
+const test = async () => {
+  await createIdentity();
+};
+
+test();
 
 export { createIdentity };

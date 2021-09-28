3bd863b547c7d71b3ab19616cc2d554e4f530594
Coodos <info@coodos.co>
2021-09-28 17:20:08 +0530

    new keypair for signing DVID

--- src/utils/onboarding.ts
+++ src/utils/onboarding.ts
@@ -1,6 +1,8 @@
 import { getConfig, writeConfig } from "./configUtil";
 import { createIssuerIdentity } from "./did";
 import { createEncryptedVault } from "./stronghold";
+import crypto from "crypto";
+import bs58 from "bs58";
 
 const startOnboarding = async (owner: string, password: string) => {
   try {
@@ -9,8 +11,22 @@ const startOnboarding = async (owner: string, password: string) => {
   } catch (error) {
     const { key, doc, receipt, updatedReceipt, mnemonic, signing } =
       await createIssuerIdentity();
+    const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 1024,
+    });
+
+    const priv = privateKey.export({ type: "pkcs1", format: "der" });
+    const pub = publicKey.export({ type: "pkcs1", format: "der" });
+
     await createEncryptedVault(
-      { didKeys: key.toJSON(), signing: signing.toJSON() },
+      {
+        didKeys: key.toJSON(),
+        signing: signing.toJSON(),
+        DVIDPair: {
+          public: bs58.encode(priv),
+          secret: bs58.encode(pub),
+        },
+      },
       "master-config",
       password
     );
